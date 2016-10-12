cb2d686c9ea748407e913c1fae7876851d900836
Todd <tmatthews0020@gmail.com>
2016-10-12 01:59:01 -0400

    fix(application) fix application folder

--- config/application.rb
+++ config/application.rb
@@ -12,8 +12,7 @@ module Blog
     # Application configuration should go into files in confiag/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    config.middleware.insert_before 0, 'Rack::Cors' do allow do origins '*' resource '*', :headers => :any, :methods => [:get, :post, :options] end end
-Read more at http://tutorials.pluralsight.com/ruby-ruby-on-rails/react-vs-angular-2-integration-with-rails#CXxOsb8Dd0w8eArq.99
+
 
   end
 end
