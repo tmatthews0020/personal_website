3d966b38486b1bb3cd496f08befbdb98c4806218
Todd <tmatthews0020@gmail.com>
2016-12-22 14:06:14 -0500

    users

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/list'
 
-  get 'users/create'
+resources :users
 
-  get 'users/update'
-
-  get 'users/delete'
-
-  get 'users/read'
 
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     get '/api' => 'application#index', defaults: { format: :json }
